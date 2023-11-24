0fb102dbe9074f42bde1197a7a7739ca0add299a
Pjus <wnstjd117@naver.com>
2023-11-24 11:04:31 +0900

    header component

--- src/components/section/Header.jsx
+++ src/components/section/Header.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
+// import { headerMenus, searchKeyword, snsLink } from "../../data/header";
+
+import Logo from '../header/Logo';
+import Menu from '../header/Menu';
+import Sns from '../header/Sns'
+
+import { Link, useLocation } from 'react-router-dom'
+
 const Header = () => {
+    const location = useLocation();
+
     return (
-        <header id='header' role='banner'>
-            header
+        <header id='header' role='banner' >
+            <Logo />
+            <Menu />
+            <Sns />
         </header>
     )
 }
